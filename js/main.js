f041b891b44491737c71aa0650ee105a02355b95
alex <alexlionstudio@gmail.com>
2017-12-16 18:18:33 +0500

    carousel_1

--- js/main.js
+++ js/main.js
@@ -52,4 +52,23 @@ $(document).ready(function() {
 	$('#top-logo a').on("click", function(){
 		fnstart();
 	});
+
+	// owlcorusel
+	$('#slider-container').owlCarousel({
+		items: 1,
+		autoplay: 4000,
+		nav: true,
+		navText: ["",""],
+		rewind: true,
+	});
+	// // кнопки появляются при наведение
+    $('.owl-stage-outer').mouseenter(function(){
+		$('.owl-next').addClass('owl-next-animate');
+		$('.owl-prev').addClass('owl-prev-animate');
+	});
+	$('#slider-container').mouseleave(function(){
+		$('.owl-next').removeClass('owl-next-animate');
+		$('.owl-prev').removeClass('owl-prev-animate');
+	});
+	
 });
