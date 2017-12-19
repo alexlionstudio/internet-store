2bb188173b624744505ce3aa0a9cd1a982e3ac55
alex <alexlionstudio@gmail.com>
2017-12-20 02:15:51 +0500

    discount-and-rewiews_1

--- js/main.js
+++ js/main.js
@@ -17,7 +17,27 @@ $(document).ready(function() {
 		$('.owl-next').removeClass('owl-next-animate');
 		$('.owl-prev').removeClass('owl-prev-animate');
 	});
+	// owlcorusel Вторая карусель в отзывах
+	$('#slider-rewiews').owlCarousel({
+		animateOut: 'zoomOutLeft',
+		animateIn: 'zoomInRight',
+		items: 1,
+		autoplay: 4000,
+		nav: true,
+		navText: ["",""],
+		rewind: true,
+	});	
+	// // кнопки появляются при наведение
+    $('#slider-rewiews').mouseenter(function(){
+		$('.owl-next').addClass('owl-next-animate');
+		$('.owl-prev').addClass('owl-prev-animate');
+	});
+	$('#slider-rewiews').mouseleave(function(){
+		$('.owl-next').removeClass('owl-next-animate');
+		$('.owl-prev').removeClass('owl-prev-animate');
+	});
 	
+
 	//  mixitup
 	var mixer = mixitup('#product-catalog');
 
@@ -38,23 +58,48 @@ $(document).ready(function() {
     });
 
 
-   	// jQuery - Калькулятор
+   	//  -------------------------------------------------
+   	//                jQuery - Калькулятор
+	//  -------------------------------------------------
 
    	// price-total - корзина
    	// price-amount - ценик
-   	// product-add - добавить продукт кнопка
-   	var PriceGet = $('#price-amount');
+
+   	// add-to-cart - добавить продукт кнопка
+   	// var test;
+   	// alert(test);
 
 
-   	function calculatePrice(){
-		var PriceGet = $('input[name=engine]:checked').val();
-		PriceGet = parseInt(PriceGet);
+   	//  отметили на странице выбранные блоки (товары)
+   	$('.add-to-cart').on('click',function(){
+   		//  Обязательно использовать селектор this иначе выбирем кучу одинаковых классов 
+   		$(this).closest('.store__card-box').attr('data-shop','choosed');
+   		var test = searchChoosedProducts();
+   		alert(test);
+   	});
+   	// Фунцция - ищем выбранные блоки -- Ниже запутался
+   	function searchChoosedProducts(finded){
+
+   		// Два варианты рабочих кодов
+   		// 1 вариант
+   		// var finded = $('.store__card-box[data-shop=choosed]').children('.store__price').data('value');
+		// 2 вариант
+		var finded = $('.store__card-box[data-shop=choosed]').children('.store__price').attr('data-value');
+		var finded = parseInt(finded);
+   		// alert(finded);
+   		return finded;
+   	}
+
+
+ //   	function calculatePrice(){
+	// 	var PriceGet = $('div[name=engine]:checked').val();
+	// 	PriceGet = parseInt(PriceGet);
 		
-		totalPrice = PriceGet;
-		// alert(modelPrice);
-		showPrice.text( ' $ ' +  addSpace(totalPrice));
-	};
-	calculatePrice();
+	// 	totalPrice = PriceGet;
+	// 	// alert(modelPrice);
+	// 	showPrice.text( ' $ ' +  addSpace(totalPrice));
+	// };
+	// calculatePrice();
 
 
 
